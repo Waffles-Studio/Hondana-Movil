cc5005acf95604d60b5121f67a4a4a237bff162e
CardChris4ever <cardchris4ever@gmail.com>
2022-12-02 10:57:15 -0600

    .
    .

---
+++ app/src/main/java/com/example/mylogin/adapter/bookAdapter.java
@@ -0,0 +1,7 @@
+package com.example.mylogin.adapter;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class bookAdapter extends  {
+
+}
---
+++ app/src/main/java/com/example/mylogin/model/Book.java
@@ -0,0 +1,20 @@
+package com.example.mylogin.model;
+
+public class Book {
+    String autor;
+    public Book(){}
+
+    public Book (String autor)
+    {
+        this.autor = autor;
+    }
+
+    public String getAutor(){
+        return autor;
+    }
+
+    public void setAutor(){
+       this.autor=autor;
+    }
+
+}
